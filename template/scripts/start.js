a76c7396587b553ab518bc5dcc8a28605a3baa59
taoqili <taoqili@126.com>
2018-03-20 15:47:35 +0800

    init

--- template/scripts/start.js
+++ template/scripts/start.js
@@ -12,7 +12,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../build/env');
 
 const fs = require('fs');
 const chalk = require('chalk');
@@ -27,9 +27,9 @@ const {
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
 const openBrowser = require('react-dev-utils/openBrowser');
-const paths = require('../config/paths');
-const config = require('../config/webpack.config.dev');
-const createDevServerConfig = require('../config/webpackDevServer.config');
+const paths = require('../build/paths');
+const config = require('../build/webpack.config.dev');
+const createDevServerConfig = require('../build/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
