600972f4c65f5aa857dff573f35ac8ac80ca2d56
taoqili <taoqili@126.com>
2018-04-04 13:55:29 +0800

    脚手架更新

--- template/scripts/upgrade.babel.js
+++ template/scripts/upgrade.babel.js
@@ -1,3 +1,8 @@
-import fs from 'fs'
-import path from 'path'
-import shelljs from 'shelljs'
+import shell from 'shelljs'
+shell.exec('git clone https://github.com/veryStarters/react-admin-starter.git')
+shell.exec('cp -a react-admin-starter/template/build/ build')
+shell.exec('cp -a react-admin-starter/template/scripts/ scripts')
+shell.exec('cp -a react-admin-starter/template/src/common/core ./src/common/')
+shell.exec('cp -a react-admin-starter/template/package.json ./new.package.json')
+shell.exec('rm -rf react-admin-starter')
+shell.exec('echo "update completed!"')
--- template/scripts/upgrade.js
+++ template/scripts/upgrade.js
@@ -1,2 +1,5 @@
+process.env.NODE_ENV = 'development'
+process.env.BABEL_ENV = 'development'
+
 require('babel-register')
 require('./upgrade.babel')
