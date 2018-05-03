7eb295b442081078d24981a3355545da83e84b04
taoqili <taoqili@126.com>
2018-05-03 16:00:09 +0800

    完善注释

--- template/scripts/upgrade.babel.js
+++ template/scripts/upgrade.babel.js
@@ -6,4 +6,6 @@ shell.exec('cp -a react-admin-starter/template/src/common/core ./src/common/')
 shell.exec('cp -a react-admin-starter/template/package.json ./package_new.json')
 shell.exec('rm -rf react-admin-starter')
 shell.exec('echo "升级成功！"')
-shell.exec('echo "PS: 升级完成后，系统根目录会增加package_new.json文件；\n为避免自动合并可能带来的冲突异常，请手工合并package.json的内容"')
+shell.exec('echo "PS: 升级完成后，系统根目录会增加package_new.json文件；' +
+  '\n为避免自动合并可能带来的冲突异常，请手工合并package.json的内容"' +
+  '\nsrc/common/layout目录不属于自动升级范围，但有可能存在脚手架相关代码更新的情况，请在更新后手动比较')
