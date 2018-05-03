c6316a722ce2a47d41006ac3a6ccd5c0ebacae62
taoqili <taoqili@126.com>
2018-05-03 15:00:58 +0800

    完善注释

--- template/src/menus.js
+++ template/src/menus.js
@@ -1,5 +1,6 @@
 /**
  * 侧边菜单配置, 需要嵌套时请使用children属性
+ * 如果此处留空，请实现api.getMenus接口
  */
 const sidebarMenus = [
   {
@@ -43,41 +44,41 @@ const sidebarMenus = [
 ]
 
 /**
- * 顶部菜单配置
+ * 顶部菜单配置, 如果留空，则无顶部菜单
  * @type {[null,null,null]}
  */
 export const topMenus = [
-  {
-    key: 'home',
-    value: '首页',
-    icon: 'home',
-    url: '/'
-  },
-  {
-    key: 'user',
-    value: '用户管理',
-    icon: 'solution',
-    url: '/user'
-  },
-  {
-    key: 'demo',
-    value: '系统用法示例',
-    icon: 'edit',
-    children: [
-      {
-        key: 'demo1',
-        value: 'demo1',
-        icon: 'edit',
-        url: '/demo/auth'
-      },
-      {
-        key: 'demo2',
-        value: 'demo2',
-        icon: 'edit',
-        url: '/demo/menutip'
-      }
-    ]
-  }
+  // {
+  //   key: 'home',
+  //   value: '首页',
+  //   icon: 'home',
+  //   url: '/'
+  // },
+  // {
+  //   key: 'user',
+  //   value: '用户管理',
+  //   icon: 'solution',
+  //   url: '/user'
+  // },
+  // {
+  //   key: 'demo',
+  //   value: '系统用法示例',
+  //   icon: 'edit',
+  //   children: [
+  //     {
+  //       key: 'demo1',
+  //       value: 'demo1',
+  //       icon: 'edit',
+  //       url: '/demo/auth'
+  //     },
+  //     {
+  //       key: 'demo2',
+  //       value: 'demo2',
+  //       icon: 'edit',
+  //       url: '/demo/menutip'
+  //     }
+  //   ]
+  // }
 ]
 
 export default sidebarMenus
