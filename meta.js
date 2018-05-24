4413f3bc74ee3b8cfacff0545564c943e81b5e25
taoqili <taoqili@126.com>
2018-05-24 17:30:42 +0800

    优化权限使用示例文档

--- meta.js
+++ meta.js
@@ -32,5 +32,5 @@ module.exports = {
     "build/dll/*.js",
     "src/**/*.js"
   ],
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}yarn install\n  yarn run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}yarn install\n  yarn start\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
